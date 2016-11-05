f45efbdc9434f29eb66f888ed9ebf9e0bb544de5
dome4 <dominic.seitz@gmx.de>
2016-11-05 02:11:11 +0100

    fixed empty opening_times table issue

--- resources/views/opening-times/partials/trafficlights.blade.php
+++ resources/views/opening-times/partials/trafficlights.blade.php
@@ -13,12 +13,17 @@
                 <a href="{!! action('DoorController@changeStatus') !!}" class="btn btn-default">schließen</a>
             </p>
             <img class="" src="/img/status/open.png" alt="Launchpad ist geöffnet">
-        @else
+        @elseif(\App\OpeningTime::getLast() != null)
             <p class="text-danger">
                 Das Launchpad ist seit {{ \App\OpeningTime::getLast()->close_at->format('H:i') }} Uhr geschlossen.
             </p>
             <p>
                 <a href="{!! action('DoorController@changeStatus') !!}" class="btn btn-default">öffnen</a>
+            </p>
+            <img class="" src="/img/status/closed.png'" alt="Launchpad ist geschlossen">
+        @else
+            <p class="text-danger">
+                Das Launchpad ist geschlossen.
             </p>
             <img class="" src="/img/status/closed.png" alt="Launchpad ist geschlossen">
         @endif

9d86587aa5033ea9129113c5db6349a391d8b0cd
lorey <lorey@users.noreply.github.com>
2016-11-11 16:15:36 +0100

    Remove buttons to open/close

--- resources/views/opening-times/partials/trafficlights.blade.php
+++ resources/views/opening-times/partials/trafficlights.blade.php
@@ -9,16 +9,10 @@
             <p class="text-success">
                 Das Launchpad ist seit {{ \App\OpeningTime::getLast()->open_at->format('H:i') }} Uhr geöffnet.
             </p>
-            <p>
-                <a href="{!! action('DoorController@changeStatus') !!}" class="btn btn-default">schließen</a>
-            </p>
             <img class="traffic-light" src="{{ asset('/img/status/open.png') }}" alt="Launchpad ist geöffnet">
         @elseif(\App\OpeningTime::getLast() != null)
             <p class="text-danger">
                 Das Launchpad ist seit {{ \App\OpeningTime::getLast()->close_at->format('H:i') }} Uhr geschlossen.
-            </p>
-            <p>
-                <a href="{!! action('DoorController@changeStatus') !!}" class="btn btn-default">öffnen</a>
             </p>
             <img class="traffic-light" src="{{ asset('/img/status/closed.png') }}" alt="Launchpad ist geschlossen">
         @else
